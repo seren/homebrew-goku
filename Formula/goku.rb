92f77322fbcee49a70e5ba881f6ca3c792e871f6
Seren <git@seren.net>
2023-02-16 11:57:39 +0100

    fix for homebrew plist_options deprecation

--- Formula/goku.rb
+++ Formula/goku.rb
@@ -5,46 +5,28 @@ class Goku < Formula
   # depends_on "https://raw.githubusercontent.com/candid82/homebrew-brew/2491ed79f9733e6d58b41eef0a771659a0eed162/joker.rb"
   desc "karabiner configurator"
   homepage "https://github.com/yqrashawn/GokuRakuJoudo"
-  url "https://github.com/yqrashawn/GokuRakuJoudo/releases/download/v0.6.0/goku.zip"
-  sha256 "f07748c2da7347a3b65826cc60827a3c770cdbb5e66fee137391668e5d1d0014"
+  url "file://Users/seren/t/goku.zip"
+  sha256 "ef7fc7ad8afbbbc965042f0c6c41adb45001fd8fe18d04a27c11da062a44dee6"
 
   def install
     bin.install "goku"
     bin.install "gokuw"
+    inreplace "service.plist", "$PLIST-NAME$", plist_name
+    inreplace "service.plist", "$OPT-BIN-PATH$", opt_bin
+    inreplace "service.plist", "$HOME-PATH$", ENV["HOME"]
+    prefix.install "service.plist"
+    prefix.install_symlink "service.plist" => "#{plist_name}.plist"
   end
 
-  plist_options :manual => "gokuw"
+  # service do
+  #   run [opt_bin/"goku"]
+  #   environment_variables {"GOKU_EDN_CONFIG_FILE" => ENV["HOME"]}/.config/karabiner.edn}
+  #   error_log_path "#{ENV["HOME"]}/Library/Logs/goku.log"
+  #   log_path "#{ENV["HOME"]}/Library/Logs/goku.log"
+  #   run_type :interval
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/goku</string>
-      </array>
-      <key>WatchPaths</key>
-      <array>
-          <string>#{ENV["HOME"]}/.config/karabiner.edn</string>
-      </array>
-      <key>EnvironmentVariables</key>
-      <dict>
-          <key>GOKU_EDN_CONFIG_FILE</key>
-          <string>#{ENV["HOME"]}/.config/karabiner.edn</string>
-      </dict>
-      <key>StandardErrorPath</key>
-      <string>#{ENV["HOME"]}/Library/Logs/goku.log</string>
-      <key>StandardOutPath</key>
-      <string>#{ENV["HOME"]}/Library/Logs/goku.log</string>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
-  end
+  #   # watch_paths: ["#{ENV["HOME"]}/.config/karabiner.edn"]
+  # end
 
   test do
     system "#{bin}/goku", "--help"
